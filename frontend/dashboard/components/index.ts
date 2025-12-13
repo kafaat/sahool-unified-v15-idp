5a81e93e1f2bbfd0474b09b15e794e4c6395168b
kafaat <80758750+kafaat@users.noreply.github.com>
2025-12-14 02:46:29 +0300

    Revert "Claude/fix sahool kernel script 01 xq f6bc nn vt fpmg jr2gm nxl"

--- frontend/dashboard/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { TaskCard } from './TaskCard'
-export { TaskList } from './TaskList'
-export { EventTimeline } from './EventTimeline'
-export { StatsCards } from './StatsCards'
-export { StatusBadge } from './StatusBadge'

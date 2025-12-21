ef1983c70d1a9ae97533f73d472211776574cb8a
Haithm Garallah <haithm,garallah@gmail.com>
2025-12-21 16:25:27 +0300

    fixed api and flutter upgrade

--- apps/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,18 +7,20 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
+import flutter_local_notifications
 import flutter_secure_storage_macos
-import isar_flutter_libs
+import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))

6daace7c4eb05d7caf0949910badcee0fb01fbe5
Haithm Garallah <haithm,garallah@gmail.com>
2026-01-06 17:28:56 +0300

    fix marketplace

--- infrastructure/core/postgres/init/02-pgbouncer-user.sql
+++ infrastructure/core/postgres/init/02-pgbouncer-user.sql
@@ -6,17 +6,18 @@
 
 -- Create pgbouncer user if it doesn't exist
 -- Password will be set to match POSTGRES_PASSWORD for compatibility with edoburu/pgbouncer image
+-- Note: The password must be set manually after creation using the same password as POSTGRES_PASSWORD
+-- This is done via the shell script 03-set-pgbouncer-password.sh
 DO $$
-DECLARE
-    postgres_password TEXT;
 BEGIN
-    -- Get POSTGRES_PASSWORD from environment (set via ALTER USER after creation)
-    -- For now, create with a placeholder that will be updated
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'pgbouncer') THEN
-        -- Create user - password will be set to POSTGRES_PASSWORD via ALTER USER
-        -- This allows edoburu/pgbouncer image to use DB_PASSWORD for auth_user connection
+        -- Create user with a temporary password
+        -- The password will be updated by 03-set-pgbouncer-password.sh to match POSTGRES_PASSWORD
         CREATE USER pgbouncer WITH PASSWORD 'temp_password_will_be_updated';
         -- Grant necessary permissions for auth_query to read pg_shadow
+        GRANT pg_monitor TO pgbouncer;
+    ELSE
+        -- If user exists, ensure it has the correct permissions
         GRANT pg_monitor TO pgbouncer;
     END IF;
 END
